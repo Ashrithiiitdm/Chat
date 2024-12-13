29f83a60cc7447db0c14492aa04f04de88e4246b
Ashrithiiitdm <cs22b2025@iiitdm.ac.in>
2024-12-13 22:05:36 +0530

    File structure init for client done

---
+++ client/src/pages/Messages.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Sidebar } from '../section/chat';
+
+export default function Messages() {
+    return (
+        <div className='h-screen overflow-hidden'>
+            <div className='h-full rounded-sm border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark xl:flex'></div>
+            <Sidebar />
+        </div>
+    )
+}
