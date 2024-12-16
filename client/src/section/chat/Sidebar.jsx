e69b02ce051c2dee816d9e0e7c5249deae4cfe14
Ashrithiiitdm <cs22b2025@iiitdm.ac.in>
2024-12-16 16:53:13 +0530

    Added more icons in Sidebar

--- client/src/section/chat/Sidebar.jsx
+++ client/src/section/chat/Sidebar.jsx
@@ -1,12 +1,54 @@
 import React from "react";
-import { Chat, SignOut } from '@phosphor-icons/react';
+import {
+    Chat,
+    DotsThreeCircle,
+    SignOut,
+    Users,
+    UserCircle,
+} from '@phosphor-icons/react';
 import DarkMode from "../../components/DarkMode";
+
+const Icons = [
+    {
+        key: 0,
+        title: 'Chat',
+        icon: <Chat size={24} />,
+    },
+    {
+        key: 1,
+        title: 'Groups',
+        icon: <Users size={24} />,
+    },
+    {
+        key: 2,
+        title: 'Profile',
+        icon: <UserCircle size={24} />,
+    },
+    {
+        key: 3,
+        title: 'More',
+        icon: <DotsThreeCircle size={24} />,
+    },
+];
+
 export default function Sidebar() {
     return (
-        <div className='flex flex-col border-r border-stroke p-2 dark:border-strokedark'>
-            <div className="mx-auto border rounded-md border-stroke p-2 dark:border-strokedark">
-                <Chat size={24} />
+        <div className="flex flex-col border-r border-stroke p-2 dark:border-strokedark">
+            <div className="flex flex-col items-center space-y-5">
+                {Icons.map((icon) => (
+                    <div
+                        key={icon.key}
+                        className="space-y-2 flex flex-col text-center hover:cursor-pointer hover:text-primary"
+                        onClick={() => { }}
+                    >
+                        <div className="mx-auto border rounded-md border-stroke p-2 dark:border-strokedark">
+                            {icon.icon}
+                        </div>
+                        <span className="font-medium text-sm">{icon.title}</span>
+                    </div>
+                ))}
             </div>
+
             <div className="flex flex-col grow"></div>
             <div className="space-y-4.5">
                 <DarkMode />
@@ -14,7 +56,6 @@ export default function Sidebar() {
                     <SignOut size={24} />
                 </div>
             </div>
-
         </div>
-    )
+    );
 }
