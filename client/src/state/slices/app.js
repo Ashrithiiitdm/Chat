24db10ae80fe5c990153c167cb9bf1bd119c378f
Ashrithiiitdm <cs22b2025@iiitdm.ac.in>
2024-12-16 23:16:36 +0530

    Modified app.js in slices

--- client/src/state/slices/app.js
+++ client/src/state/slices/app.js
@@ -2,20 +2,16 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initState = {
     modals: {
-        gif: false,
+
     },
-    selectedGifUrl: '',
+
 };
 
 const slice = createSlice({
     name: 'app',
     initialState: initState,
     reducers: {
-        updateGifModal(state, action) {
-            state.modals.gif = action.payload.value;
-            state.selectedGifUrl = action.payload.url;
 
-        },
     },
 });
 
